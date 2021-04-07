7bf996664bc98002c99c638b7f26042d81fba99d
Turan Ege Caner <ecaner@sabanciuniv.edu>
2021-04-08 02:31:22 +0300

    Product Model

---
+++ Models/Product.cs
@@ -0,0 +1,18 @@
+namespace OnlineStoreProject.Models
+{
+    public class Product
+    {
+        public int ProductId{get; set;}
+        public string ProductName{get; set;}
+        public int CategoryId{get; set;}
+        public Nullable<string> Description{get; set;}
+        public Nullable<string> ImageUrl{get; set;}
+        public Nullable<System.DateTime> CreateDate{get; set;}
+        public Nullable<System.DateTime> ModifyDate{get; set;}
+        public Nullable<int> Quantity{get; set;}
+        public Nullable<bool> IsActive{get; set;}
+        public Nullable<bool> IsDelete {get; set;}
+        public Nullable<double> rating{get; set;}
+        public Nullable<decimal> price{get; set;}
+    }
+}
