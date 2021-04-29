64a97774025a4cdd83f6a0e79185f345faa93423
s21yilmazselcuk <yilmazselcuk@sabanciuniv.edu>
2021-04-29 16:57:23 +0300

    Database Conversion to Remote

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,13 +1,31 @@
 using OnlineStoreProject.Models;
 using Microsoft.EntityFrameworkCore;
-
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using Pomelo.EntityFrameworkCore.MySql;
 namespace OnlineStoreProject.Data.DataContext
 {
     public class DataContext : DbContext
     {
+    public DataContext()
+    {
+    }
+     //public virtual Microsoft.EntityFrameworkCore.DbContextOptionsBuilder UseLoggerFactory (Microsoft.Extensions.Logging.ILoggerFactory loggerFactory);
+     //public partial void LoggerFactory (System.Collections.Generic.IEnumerable<Microsoft.Extensions.Logging.ILoggerProvider> providers, Microsoft.Extensions.Logging.LoggerFilterOptions filterOptions);
      public DataContext(DbContextOptions<DataContext> options): base(options){}
-     public DbSet <Customer> Customers {get; set;}
-     public DbSet <Product> Products {get; set;}
-     public DbSet <Comment> Comments {get; set;}
+     public virtual DbSet <Customer> Customers {get; set;}
+     public virtual DbSet <Product> Products {get; set;}
+     public virtual DbSet <Comment> Comments {get; set;}
+     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+         optionsBuilder
+                .UseMySql(   "server=remotemysql.com;port=3306;user=e9kvb80H9D;password=moKwFVzbog;database=e9kvb80H9D;",
+            new MySqlServerVersion(new Version(8, 0, 11)))
+                .UseLoggerFactory(LoggerFactory.Create(b => b
+                .AddConsole()
+                .AddFilter(level => level <= LogLevel.Information)))
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+     }
     }
 }
