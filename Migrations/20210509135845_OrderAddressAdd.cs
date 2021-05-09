5444362572379e16924943cec121385d920ad4cc
Turan Ege Caner <ecaner@sabanciuniv.edu>
2021-05-09 16:59:36 +0300

    migration

---
+++ Migrations/20210509135845_OrderAddressAdd.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineStoreProject.Migrations
+{
+    public partial class OrderAddressAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Products",
+                newName: "ProductId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Customers",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldDefaultValue: "Customer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "Products",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Role",
+                table: "Customers",
+                type: "longtext",
+                nullable: false,
+                defaultValue: "Customer",
+                oldClrType: typeof(string),
+                oldType: "longtext");
+        }
+    }
+}
